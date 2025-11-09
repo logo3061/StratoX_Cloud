ee5fdb6ffc59dfe1182cd29232031e564f5c9401
Hunterduncan362 <paragonia.deepworks@gmail.com>
2025-11-09 13:42:55 +0100

    Mobile V3

--- pages/js/app.js
+++ pages/js/app.js
@@ -84,3 +84,9 @@ initStars();
 drawStars();
   })();
 })();
+const btn = document.getElementById('mobile-menu-btn');
+  const menu = document.getElementById('mobile-menu');
+
+  btn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
