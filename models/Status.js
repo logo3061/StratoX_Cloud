fc5f1f71cf71546d31d87bd464ed6c4128ac8083
logo3061 <leonzw10@gmail.com>
2026-02-07 15:17:07 +0100

    f

--- models/Status.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const statusSchema = new mongoose.Schema({
-    state: { type: String, default: "PRE-ALPHA" },
-    obsidian: { type: String, default: "OPERATIONAL" },
-    lastUpdated: { type: String, default: () => new Date().toLocaleString('de-DE') }
-});
-
-module.exports = mongoose.model('Status', statusSchema);
